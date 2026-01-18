7c85dababa539cc44cccaf12157e8ad68788a391
Ayodele Ajisegiri <ayodeleajisegiri@gmail.com>
2026-01-18 23:57:40 +0100

    Fix relationship_app views for class-based view task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 
-from relationship_app.models import Book, Library
+from .models import Book, Library
 
 
-# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
-# Class-based view: display library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
