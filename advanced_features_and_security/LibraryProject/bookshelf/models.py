b805cfbebc56fdc3d00d50c41c27022fccae269e
Ayodele Ajisegiri <ayodeleajisegiri@gmail.com>
2026-01-25 00:05:37 +0100

    Implement permissions and groups for role-based access ctrl

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,6 +3,28 @@ from django.conf import settings
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def view_resource(request):
+    return render(request, "view.html")
+
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_resource(request):
+    return render(request, "create.html")
+
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit_resource(request):
+    return render(request, "edit.html")
+
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_resource(request):
+    return render(request, "delete.html")
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
@@ -42,3 +64,19 @@ class Book(models.Model):
         return self.title
 
 
+from django.db import models
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        permissions = (
+            ("can_view", "Can view resource"),
+            ("can_create", "Can create resource"),
+            ("can_edit", "Can edit resource"),
+            ("can_delete", "Can delete resource"),
+        )
