e0703ecede958b4c7b59c3226c9af88e4e8da511
Ayodele Ajisegiri <ayodeleajisegiri@gmail.com>
2026-01-25 00:27:02 +0100

    Implement

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -32,3 +32,29 @@ Permissions are enforced in views using Django's
 
 Example:
 @permission_required("relationship_app.can_edit", raise_exception=True)
+
+## Permissions and Groups Setup
+
+This project demonstrates role-based access control using
+Django permissions and groups.
+
+### Custom Permissions
+Defined in `bookshelf/models.py`:
+- can_view
+- can_create
+- can_edit
+- can_delete
+
+### Groups
+- Viewers: can_view
+- Editors: can_view, can_create, can_edit
+- Admins: can_view, can_create, can_edit, can_delete
+
+### Enforcement
+Permissions are enforced in `bookshelf/views.py` using Django’s
+`@permission_required` decorator with `raise_exception=True`.
+
+Example:
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Resource
 
-# Create your views here.
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def book_list(request):
+    books = Resource.objects.all()
+    return render(request, "books.html", {"books": books})
+
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create_book(request):
+    return render(request, "create_book.html")
+
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit_book(request, book_id):
+    return render(request, "edit_book.html")
+
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete_book(request, book_id):
+    return render(request, "delete_book.html")
