c4c7f15794dd10f5fc260ee48556b30bc9176057
Ayodele Ajisegiri <ayodeleajisegiri@gmail.com>
2026-02-13 03:07:34 +0100

    Clean forms.py and configure TagWidget properly

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,37 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post
-from .models import Comment
-from .models import Post, Tag
+from taggit.forms import TagWidget
 
-
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="Comma-separated tags")
-
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
-
-    def save(self, commit=True):
-        instance = super().save(commit=False)
-
-        if commit:
-            instance.save()
-
-            tags_data = self.cleaned_data['tags']
-            tag_list = [tag.strip() for tag in tags_data.split(',') if tag.strip()]
-
-            instance.tags.clear()
-
-            for tag_name in tag_list:
-                tag, created = Tag.objects.get_or_create(name=tag_name)
-                instance.tags.add(tag)
-
-        return instance
-
+        widgets = {
+            'tags': TagWidget(),
+        }
 
 
 class RegisterForm(UserCreationForm):
@@ -39,13 +20,14 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["username", "email"]
+        fields = ['username', 'email']
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
